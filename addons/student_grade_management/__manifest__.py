5d721c8e87fe54b9b09a012068fbeeb98c1a417d
Tuan Nguyen <famaxlevel1997@gmail.com>
2023-08-21 14:39:49 +0700

    build: init student_grade_management module

---
+++ addons/student_grade_management/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Quản lý điểm của sinh viên",
+    "version": "1.0",
+    "author": "Tuan Nguyen - 1041360309",
+    "summary": "[HAUI] Quản lý điểm của sinh viên",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/student_view.xml",
+    ],
+    "installable": True,
+}
